f79775f4e8b44c678adcb3e6b803182df52c3660
major-phyo-san <phyojupiter9@gmail.com>
2025-02-19 17:23:40 +0700

    cpanel deployment

---
+++ symlink.php
@@ -0,0 +1,15 @@
+<?php
+
+// Adjust the paths based on your project structure
+$laravelPath = '/home/zawpszw2/public_html';
+$appPath = $laravelPath . '/storage/app/public';
+$linkPath = $laravelPath . '/storage';
+
+// Check if the symbolic link doesn't exist
+if (!is_link($linkPath)) {
+    // Create the symbolic link
+    symlink($appPath, $linkPath);
+    echo 'Symbolic link created successfully.';
+} else {
+    echo 'Symbolic link already exists.';
+}
